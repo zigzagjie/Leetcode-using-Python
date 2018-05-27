1984f62e520ec06538f48793fa9197a806de1d24
zigzagjie <31840384+zigzagjie@users.noreply.github.com>
2018-05-27 08:29:19 +0800

    Create Easy-21. Merge Two Sorted Lists.py

---
+++ Easy/Easy-21. Merge Two Sorted Lists.py
@@ -0,0 +1,12 @@
+"""
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+Example: 
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+"""
+
+##linked List is a special data structure.
+
+#https://www.cnblogs.com/cq146637/p/8064922.html
+#http://www.cnblogs.com/yupeng/p/3413763.html
+
